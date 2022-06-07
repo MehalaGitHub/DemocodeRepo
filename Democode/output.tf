47c5b76770f9a9393423ba7467f9e11c6f54b019
MehalaGitHub <106929090+MehalaGitHub@users.noreply.github.com>
2022-06-07 20:10:41 +0100

    add democodetffiles

---
+++ Democode/output.tf
@@ -0,0 +1,7 @@
+output "rg_group_name" {
+    value="${azurerm_resource_group.devops_res_group.name}"
+}
+
+output "rg_group_location" {
+    value="${azurerm_resource_group.devops_res_group.location}"
+}
---
+++ Democode/variables.tf
@@ -0,0 +1,12 @@
+variable "rg_mehala" {
+    type = string
+    default="rg_mehala"
+    description = "Name of the resource group"
+  
+}
+
+variable "rg_location" {    
+    default="West Europe"
+    description = "Name of the location where to be deployed"
+  
+}
